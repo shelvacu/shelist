556103f8f73098f3d551f09bb93140ded35fd4de
Shelvacu <shelvacu@gmail.com>
2015-05-11 01:05:48 +0000

    added remove images

--- main.js
+++ main.js
@@ -1,44 +1,64 @@
 if(!localStorage.getItem("listItems")){
     //init
-    localStorage.setItem("listItems",[]);
+    localStorage.setItem("listItems",'[]');
 }
 $(document).ready(function(){
     console.log('w00t w00t');
+    function update(idx,checked,itemName){
+	console.log("idx is", idx);
+	if(typeof idx != 'number')
+	    idx = $(idx).data("index");
+	l = JSON.parse(localStorage.getItem("listItems"));
+	if(typeof  checked != 'undefined')
+	    l[idx][0] = checked;
+	if(typeof itemName != 'undefined')
+	    l[idx][1] = itemName;
+	localStorage.setItem("listItems",JSON.stringify(l));
+    }
     function addItem(itemName,checked,fromLocal){
 	//both checked and fromLocal default to false, and undefined is falsy!
 	checked   = !!checked
 	fromLocal = !!fromLocal
 	c = $("#template-row").clone();
+	idx = $("#template-row").data("index");
+	c.data("index",idx);
+	$("#template-row").data("index",idx+1);
 	c.attr("id","");
 	c.css("visibility","visible");
 	if(!itemName) itemName = $("#add-input").val();
 	c.find(".item-desc").text(itemName);
 	b = c.find(".bigcheck");
-	b.on('mousedown',function(e){
+	b.on('mousedown', null, b, function(e){
+	    b = e.data;
+	    console.log(b,b.data());
 	    if( typeof b.data("checked") === 'undefined' )
 		b.data("checked",false);
 	    if(!b.data("checked")){
 		b.children("img").attr("src","inset.png");
 	    }
-	}).on('mouseup',function(e){
-	    if(b.data("checked")){
-		b.children("img").attr("src","unchecked.png");
-		b.data("checked",false);
-	    }else{
-		b.children("img").attr("src","checked.png");
-		b.data("checked",true);
-	    }
+	}).on('mouseup', null, b, function(e){
+	    b = e.data;
+	    isChk = !b.data("checked")
+	    b.data("checked",isChk);
+	    src = (isChk ? "checked.png" : "unchecked.png");
+	    console.log(src,b.children("img"));
+	    b.children("img").attr("src",src);
+	    update(b.parents("tr:first"),isChk);
 	});
-	if(checked) b.attr("src","checked.png"); //it defaults to unchecked, so no else needed here.
+	if(checked) b.children('img').attr("src","checked.png");
 	if(!fromLocal){
-	    l = localStorage.getItem("listItems");
-	    l.push(checked,itemName);
-	    localStorage.setItem("listItems",l);
+	    l = JSON.parse(localStorage.getItem("listItems"));
+	    l.push([checked,itemName]);
+	    localStorage.setItem("listItems",JSON.stringify(l));
 	}
 	c.insertBefore($("#add-item-row"));
 	$("#add-input").val("");
     }
-    localStorage.getItem("listItems").foreach(function(ob){addItem(ob[1],ob[0])});
+    console.log(localStorage.getItem("listItems"));
+    JSON.parse(localStorage.getItem("listItems")).forEach(function(ob){
+	addItem(ob[1],ob[0],true);
+    });
+
     $("#add-input").on('keydown',function(e){
 	if(e.which == 13)
 	    addItem();
